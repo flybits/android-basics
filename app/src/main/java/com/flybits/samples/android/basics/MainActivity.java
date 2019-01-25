16445ed2da62b2c3bf365c4c363782e53f687acc
Petar Kramaric <pkramaric@flybits.com>
2019-01-25 10:36:20 -0500

    Add layout for connection screen

--- app/src/main/java/com/flybits/samples/android/basics/MainActivity.java
+++ app/src/main/java/com/flybits/samples/android/basics/MainActivity.java
@@ -3,12 +3,39 @@ package com.flybits.samples.android.basics;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connect);
+
+        Button btnConnect = findViewById(R.id.btnConnect);
+        Button btnDisconnect = findViewById(R.id.btnDisconnect);
+        Button btnIsConnected = findViewById(R.id.btnIsConnected);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnIsConnected.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

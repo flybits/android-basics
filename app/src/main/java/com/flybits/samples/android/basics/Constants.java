391473d6cef128d2a7540cf4ba83a05cb18f84a5
Petar Kramaric <pkramaric@flybits.com>
2019-12-18 11:12:20 -0500

    Remove project id from constants

--- app/src/main/java/com/flybits/samples/android/basics/Constants.java
+++ app/src/main/java/com/flybits/samples/android/basics/Constants.java
@@ -2,6 +2,6 @@ package com.flybits.samples.android.basics;
 
 public class Constants {
 
-    public static final String PROJECT_ID = "8E9A30FD-D138-46B2-AA66-508A6E79F5E7";
+    public static final String PROJECT_ID = "";
 
 }
